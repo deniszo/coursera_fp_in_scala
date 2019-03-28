dce73d668fd64ac567399d36f9fae547d0e66fdc
Denis Zolkin <dezolkin@gmail.com>
2019-03-28 18:08:05 +0100

    [fp_principles][week1] solutions

--- fp_principles/week1/src/main/scala/recfun/Main.scala
+++ fp_principles/week1/src/main/scala/recfun/Main.scala
@@ -13,14 +13,52 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+    def pascal(c: Int, r: Int): Int =
+      if (c <= 0 || r <= 0 || c == r) {
+        1
+      } else {
+        pascal(c, r -1) + pascal(c-1, r - 1)
+      }
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ??
+    def balance(chars: List[Char]): Boolean = {
+      def matchP(parens: List[Char], rawChars: List[Char]): Boolean = {
+        if (rawChars.isEmpty) {
+          parens.isEmpty
+        } else {
+          rawChars.head match {
+            case '(' => matchP(rawChars.head :: parens, rawChars.tail)
+            case ')' =>
+              if (parens.isEmpty) {
+                false
+              } else {
+                matchP(parens.tail, rawChars.tail)
+              }
+            case _ => matchP(parens, rawChars.tail)
+          }
+        }
+      }
+
+      matchP(List[Char](), chars)
+    }
   
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ??
+    def countChange(money: Int, coins: List[Int]): Int = {
+      def count(waysNum: Int, money: Int, coins: List[Int]): Int =
+        if (money <= 0 || coins.isEmpty) {
+          waysNum
+        } else if (coins.head > money) {
+          count(waysNum, money, coins.tail)
+        } else if (coins.head == money) {
+          count(waysNum + 1, money, coins.tail)
+        } else {
+          count(waysNum, money - coins.head, coins) + count(0, money, coins.tail)
+        }
+
+      count(0, money, coins)
+    }
+  }
