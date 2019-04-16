566505f27dc46d88f72867336c13fe56b733bc25
Denis Zolkin <dezolkin@gmail.com>
2019-04-16 17:42:17 +0200

    [fp_principles][week4] subtyping + variance -> Var and Prod

--- fp_principles/week4/src/main/scala/pm/Pm.scala
+++ fp_principles/week4/src/main/scala/pm/Pm.scala
@@ -2,15 +2,30 @@ package pm
 
 trait Expr
 case class Number(n: Int) extends Expr
+case class Var(name: String) extends Expr
 case class Sum(e1: Expr, e2: Expr) extends Expr
+case class Prod(e1: Expr, e2: Expr) extends Expr
 
 object exprs extends App {
   def show(e: Expr): String = e match {
     case Number(x) => x.toString
+    case Var(name) => name
     case Sum(l, r) => show(l) + " + " + show(r)
+    case Prod(l, r) => {
+        def placeParens(e: Expr): String = e match {
+          case Sum(_, _) => "(" + show(e) + ")"
+          case _ => show(e)
+        }
+
+        placeParens(l) + " * " + placeParens(r)
+    }
   }
 
   override def main(args: Array[String]): Unit = {
     println(show(Sum(Number(1), Number(44))))
+
+    println(show(Sum(Prod(Number(2), Var("x")), Var("y"))))
+
+    println(show(Prod(Sum(Number(2), Var("x")), Var("y"))))
   }
 }
