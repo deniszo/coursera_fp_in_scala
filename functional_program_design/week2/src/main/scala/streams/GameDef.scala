8e579bba8251aa834aecbf0a8fb55da553ce3eba
Denis Zolkin <dezolkin@gmail.com>
2019-05-08 17:41:22 +0200

    [functional_program_design][week2] game methods

--- functional_program_design/week2/src/main/scala/streams/GameDef.scala
+++ functional_program_design/week2/src/main/scala/streams/GameDef.scala
@@ -81,7 +81,7 @@ trait GameDef {
    * This function returns the block at the start position of
    * the game.
    */
-  def startBlock: Block = ???
+  def startBlock: Block = Block(startPos, startPos)
 
 
   /**
@@ -132,22 +132,22 @@ trait GameDef {
      * Returns the list of blocks that can be obtained by moving
      * the current block, together with the corresponding move.
      */
-    def neighbors: List[(Block, Move)] = ???
+    def neighbors: List[(Block, Move)] = List((left, Left), (right, Right), (up, Up), (down, Down))
 
     /**
      * Returns the list of positions reachable from the current block
      * which are inside the terrain.
      */
-    def legalNeighbors: List[(Block, Move)] = ???
+    def legalNeighbors: List[(Block, Move)] = this.neighbors.filter({ case(newBlock, _) => newBlock.isLegal})
 
     /**
      * Returns `true` if the block is standing.
      */
-    def isStanding: Boolean = ???
+    def isStanding: Boolean = this.b1.col == this.b2.col && this.b1.row == this.b2.row
 
     /**
      * Returns `true` if the block is entirely inside the terrain.
      */
-    def isLegal: Boolean = ???
+    def isLegal: Boolean = terrain(this.b1) && terrain(this.b2)
   }
 }
